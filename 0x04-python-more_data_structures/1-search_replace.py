6ff01bbe0db5d72b36b1673f945515d013031d79
Njeriwaweru <njeripraise318@gmail.com>
2022-07-11 12:34:18 +0000

    freplaces

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+
